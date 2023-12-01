11fbdc3956b9210245bfe5ce6d35ea75ebb8d558
bodin <bodinsonsawan@gmail.com>
2023-12-01 13:35:06 +0700

    fix(ex3)

--- ex-3.js
+++ ex-3.js
@@ -1,23 +1,13 @@
 // Exercise #3: Min Numbers
 let numbers = [100, 20, 3, 1000];
-let minNumber = 0;
+let minNumber;
+
+minNumber = numbers[0];
 
 for (let i = 0; i < numbers.length; i++) {
-  if (minNumber === 0 || numbers[i] < minNumber) {
+  if (numbers[i] < minNumber) {
     minNumber = numbers[i];
   }
 }
 
 console.log(minNumber);
-
-//   if (minNumber === 0 || minNumber > numbers[i]) {
-// รอบ1   0 > 100 = false  or minNumber === 0 = true -> re-as minNumber = 100
-// รอบ2  100 > 20 = true -> re-as minNumber= 20
-// รอบ3 20 > 3 = true -> re-as minNumber=3
-// รอบ4 3 > 100 = false -> minNumber=3
-
-//  if (minNumber === 0 || numbers[i] < minNumber) {
-// รอบ1   100 < 0 = false  or minNumber === 0 = true -> re-as minNumber = 100
-// รอบ2  20 < 100 = true -> re-as minNumber= 20
-// รอบ3 3 < 20 = true -> minNumber=3
-// รอบ4 1000 < 3 = false ->  minNumber= 3
