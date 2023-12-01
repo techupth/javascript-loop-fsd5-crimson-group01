4ed93da885a170e963bf388a31da92f50e08e5e8
bodin <bodinsonsawan@gmail.com>
2023-12-01 11:33:33 +0700

    fix(ex1-6):bodin's assignment

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,10 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
 
-// Start coding here
+for (let i = 0; i < companyName.length; i++) {
+  console.log(`Number ${i + 1} character is ${companyName[i]}`);
+}
+
+//รอบ1 ${i + 1} เพื่อให้ ค่า i ตั้งต้น 0+1 = ในรอบแรกมีค่าเป็น 1
+//รอบ2 ${i + 1} เพื่อให้ ค่า i= 1+1=2 ในรอบ2มีค่าเป็น 2
+//รอบ3 ${i + 1} เพื่อให้ ค่า i= 2+1=3 ในรอบ3มีค่าเป็น 3
